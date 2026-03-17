f6ea395121aedada18d62eb1bd8d8c4c81243b71
Guidjhonsen <guidjhonsen.tu.rey@gmail.com>
2026-03-17 10:20:40 -0400

    Retorna consulta de Fecha

--- src/main/java/com/mitocode/repo/IConsultRepo.java
+++ src/main/java/com/mitocode/repo/IConsultRepo.java
@@ -4,6 +4,7 @@ import com.mitocode.model.Consult;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface IConsultRepo extends IGenericRepo<Consult, Integer>{
@@ -13,4 +14,7 @@ public interface IConsultRepo extends IGenericRepo<Consult, Integer>{
 
     @Query("FROM Consult c WHERE c.patient.dni=:dni OR LOWER(c.patient.firstName) LIKE %:fullname% OR LOWER(c.patient.lastName) LIKE %:fullname%")
     List<Consult> search(@Param("dni") String dni,@Param("fullname") String fullname);
+
+    @Query("FROM Consult c WHERE c.consultDate BETWEEN :date1 AND :date2")
+    List<Consult> seachrByDate(@Param("date1")LocalDateTime date1, @Param("date2")LocalDateTime date2);
 }
